e1a7dbd90ab760eeb79e877b6c2c2569efc40c99
ngc <feizhaoye@gmail.com>
2018-12-12 00:45:33 +0800

    add some fix and api doc

--- hotel_manage/do_query.py
+++ hotel_manage/do_query.py
@@ -21,9 +21,18 @@ __mtime__ = '2018/12/4'
 """
 from .models import *
 from datetime import date
+import django
 
 
-class check_user():
+"""
+数据库交互脚本，所有关于数据库的操作封装在这里
+"""
+
+class check_user:
+    """
+    检查用户的类，用于查看用户是否存在，获取其密码，以及获取其是否为管理员，
+    初始化类后必须调用check_user，若返回true才可以进行下一步操作
+    """
     def __init__(self):
         self.user_query = None
     
@@ -42,14 +51,26 @@ class check_user():
 
 
 def create_user(username, password):
+    """
+    创建用户
+    :param username:
+    :param password:
+    :return:
+    """
     try:
         User(user_username=username, user_password=password, is_admin=False).save()
         return True
     except Exception as e:
         print(e)
         return False
-    
+
+
 def get_index_data(is_admin):
+    """
+    获取主页面的数据
+    :param is_admin: 是否为主管理员，用于渲染控制部分
+    :return:
+    """
     orders = []
     total_money = 0
     total_live_room = 0
@@ -72,15 +93,15 @@ def get_index_data(is_admin):
         total_money += Room.objects.get(id=order.room_id).room_price
     
     valid_room = total_room - total_live_room
-    live_percent = str(total_live_room/total_room*100).split(".")[0]+"%"
+    live_percent = str(total_live_room / total_room * 100).split(".")[0] + "%"
     recv_data = {
         't_live': total_live_room,
         'live_percent': live_percent,
         'valid': str(valid_room),
         'orders': orders,
-        'y_money':total_money,
-        'm_money':total_money,
-        'is_admin':is_admin,
+        'y_money': total_money,
+        'm_money': total_money,
+        'is_admin': is_admin,
         'bills': [{
             'date': today_date,
             'money': total_money,
@@ -89,5 +110,79 @@ def get_index_data(is_admin):
         }]
     }
     return recv_data
-    
-    
+
+
+def check_room_status(room_number):
+    """
+    获取房间状态（今日是否有人住在这个房间中）
+    :param room_number: 房间编号
+    :return:
+    """
+    try:
+        room = Room.objects.get(room_number=room_number)
+        not_f_order = Order.objects.filter(room=room, is_finish=False)
+        live_number = 0
+        live_guest = []
+        for order in not_f_order:
+            if order.guest not in live_guest:
+                live_guest.append(order.guest)
+                live_number += 1
+            else:
+                continue
+        room_capcity = room.room_type.capacity
+        if room_capcity > live_number:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(e)
+        return False
+
+
+def add_guest(guest_name, guest_c_id):
+    """
+    增加顾客，若存在顾客，直接返回guest类，若不存在，创建后返回
+    :param guest_name:
+    :param guest_c_id:
+    :return:
+    """
+    try:
+        guest = Guest(guest_name=guest_name, guest_c_id=guest_c_id)
+        guest.save()
+        return guest
+    except django.db.utils.IntegrityError:
+        guest = Guest.objects.get(guest_name=guest_name, guest_c_id=guest_c_id)
+        return guest
+    except Exception as e:
+        print(e)
+        return False
+
+
+def create_order(guest_name, guest_c_id, room_number, order_date):
+    """
+    创建订单
+    :param guest_name:
+    :param guest_c_id:
+    :param room_number:
+    :param order_date:
+    :return:
+    """
+    try:
+        guest = add_guest(guest_name, guest_c_id)
+        if not check_room_status(room_number):
+            return False
+        else:
+            room = Room.objects.get(room_number=room_number)
+        today_date = date.today()
+        year, month, day = order_date.split("-")
+        order_date = date(int(year), int(month), int(day))
+        if order_date < today_date:
+            return False
+        elif order_date > today_date:
+            Order(guest=guest, room=room, is_reverse=True, reverse_date=order_date.isoformat()).save()
+        else:
+            Order(guest=guest, room=room).save()
+        return True
+    except Exception as e:
+        print(e)
+        return False
--- readme.md
+++ readme.md
@@ -9,3 +9,144 @@ python manage.py migrate
 python manage.py makemigrations
 
 ```
+
+
+## api
+
+date形式 xxxx-xx-xx iso时间格式
+
+check_in
+```
+requests
+{
+	'name':'xxx',
+	'id_number':'',
+	'room_number':'',
+	'date':'xxxx-xx-xx'
+}
+response
+{
+	'status':['seccuss'.'error'],
+	'msg':'xxx'	
+}
+```
+
+settle
+结账分为2部分，首先查找未完成的账单，其次结账
+可分为两个api
+```
+requests
+{
+	'name':'',
+	'id_number':'xxxx'
+}
+response
+{
+	'id':'',
+	'id_number':'',
+	'name':'',
+	'room_number':'',
+	'date':'',
+	'price':''
+}
+```
+
+settle_finish
+```
+requests
+['xx','xx']
+response
+{
+	'status':'xxx',
+	'msg':'xxxx'
+}
+```
+
+group_book
+```
+requests
+{
+	'org_name':'xx',
+	'member':[{
+		'name':'',
+		'id_number':'',
+		'room_number':''
+	}
+	...],
+	'date':''
+}
+response
+{
+	'status':['seccuss'.'error'],
+	'msg':'xxx'	
+}
+```
+
+group_settle
+```
+requests
+{
+	'org_name':'xx',
+}
+response
+{
+	'orders':[{
+		'id':'xx',
+		'id_number':'xxx',
+		'name':'xxx',
+		'room_number':'xx',
+		'date':'xxx',
+		'price':'xxx',
+}
+```
+
+group_settle_finsih
+
+```
+requests
+['xx','xx']
+response
+{
+	'status':'xxx',
+	'msg':'xxxx'
+}
+```
+
+check_guest_info
+
+```
+requests
+{
+	'name':['xxx','xxx'],
+	'id_number':['xxxx','xxxx'],
+	'room_number:['xxx','xxxx']
+}
+response
+{
+	'guests_info':[{
+		'name':'xxx',
+		'id_number':'xxx',
+		'is_live':'xxx',
+		'last_live_room':'',
+	}]
+}
+```
+
+check_room
+
+```
+get
+
+response
+{
+	'room_info':[{
+		'room_number':'xxx',
+		'room_type':'xxx',
+		'is_live':'xxx',
+		'is_book':'xxx',
+		'room_price':'',
+	}]
+}
+```
+
+change_room 你自己想想吧，我不知道怎么写了
